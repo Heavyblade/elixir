11979d8475f9bada594afe6b39640cc50b8596d1
CristianV <cristian.vasquez@educate-online.com>
2016-11-09 22:53:11 -0500

    Adding project deps

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -8,7 +8,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-      parse_args(argv)
+      argv
+      |> parse_args
+      |> process
   end
 
   @doc """
@@ -22,11 +24,22 @@ defmodule Issues.CLI do
       parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
 
       case parse do
-            { [help: true], _, _ } -> :help
-            { _, [user, project, count], _ } -> { user, project, count}
+            { [help: true], _, _ }           -> :help
+            { _, [user, project, count], _ } -> { user, project, String.to_integer(count) }
             { _, [user, project], _ }        -> { user, project, @default_count }
             _ -> :help
       end
+  end
+
+
+  def process(:help) do
+      IO.puts """
+      usage: issues <user> <project> [ count | #{ @default_count } ]
+      """
+  end
+
+  def process({user, project, _count}) do
+      Issues.GithubIssues.fetch(user, project)
   end
 
 end
