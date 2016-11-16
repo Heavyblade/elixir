73f094c2d96d4dcbb87ef57dfe76509b42c0b42b
CristianV <cristian.vasquez@educate-online.com>
2016-11-15 23:37:29 -0500

    Adding print table

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -69,9 +69,16 @@ defmodule Issues.CLI do
       max_created_element = Enum.max_by( list, fn(x) -> x |> Map.fetch!("created_at") |> String.length end ) |> Map.fetch!("created_at") |> String.length
       max_title_element   = Enum.max_by( list, fn(x) -> x |> Map.fetch!("title") |> String.length end ) |> Map.fetch!("title") |> String.length
 
-      IO.puts max_id_element
-      IO.puts max_created_element
-      IO.puts max_title_element
+      IO.puts fill_blanks("", max_id_element, "-") <> " + " <> fill_blanks("", max_created_element, "-") <> " + " <> fill_blanks("", max_title_element, "-")
+      IO.puts fill_blanks("#", max_id_element) <> " | " <> fill_blanks("created_at", max_created_element) <> " | title"
+      IO.puts fill_blanks("", max_id_element, "-") <> " + " <> fill_blanks("", max_created_element, "-") <> " + " <> fill_blanks("", max_title_element, "-")
+
+      Enum.each list,
+                fn(item) -> IO.puts Enum.join([Map.fetch!(item, "id"), Map.fetch!(item, "created_at"), Map.fetch!(item,"title")], " | ") end
+  end
+
+  def fill_blanks(string, size, fill_with \\ " ") do
+      Enum.join([string] ++ Enum.map(1..(size - String.length(string)), fn _x -> fill_with end), "")
   end
 
 end
