6889e677f8e7eb97d572d7ebb3169539fad8cda1
CristianV <cristian.vasquez@educate-online.com>
2017-05-07 22:51:44 -0500

    Adding text

--- code_poster/lib/new_code_poster.ex
+++ code_poster/lib/new_code_poster.ex
@@ -1,8 +1,9 @@
 defmodule NewCodePoster do
+  require Logger
 
   def execute(code_path, image_path, ratio) do
-      code   loadcode(code_path)
-      image = d_image(image_path)
+      code  = load_code(code_path)
+      image = load_image(image_path)
       construct_text_elements(code, ratio, image)
   end
 
@@ -23,8 +24,8 @@ defmodule NewCodePoster do
   end
 
   def load_code(path) do
-      Logger.debug("Loading code from '#{code_path}'...")
-      read_folder(code_path)
+      Logger.debug("Loading code from '#{path}'...")
+      read_folder(path)
        |> clean_code
        |> String.codepoints
   end
@@ -35,13 +36,54 @@ defmodule NewCodePoster do
       image
   end
 
-  def construct_text_elements(code, ratio, %{width: width, pixels: pixels}) do
+  @doc """
+  iex> ElixirPoster.to_hex {50, 150, 250}
+  "#3296FA"
+  iex> ElixirPoster.to_hex {255, 0, 128}
+  "#FF0080"
+  """
+  def to_hex({r, g, b}) do
+      "#" <>
+      (r |> :binary.encode_unsigned |> Base.encode16) <>
+      (g |> :binary.encode_unsigned |> Base.encode16) <>
+      (b |> :binary.encode_unsigned |> Base.encode16)
+  end
+
+  @doc """
+  iex> ElixirPoster.to_hex {50, 150, 250}
+  "#3296FA"
+  iex> ElixirPoster.to_hex {255, 0, 128}
+  "#FF0080"
+  """
+  def to_hex({r, g, b, a}) do
+      "#" <>
+      (r |> :binary.encode_unsigned |> Base.encode16) <>
+      (g |> :binary.encode_unsigned |> Base.encode16) <>
+      (b |> :binary.encode_unsigned |> Base.encode16) <>
+      (a |> :binary.encode_unsigned |> Base.encode16)
+  end
+
+  def pixed_mapper({r,g,b}, x, y) do
+      { :text, %{x: 100, y: 100, fill: "#{to_hex(r,g,b)}"}, chr }
+  end
+
+  def row_mapper([], restCode, row_mapped, x, y), do: {row_mapped, restCode}
+
+  def row_mapper([pixel | rest_row], [ chr | restCode ], row_mapped, x, y) do
+       pixel = pixed_mapper(pixel, chr, x, y)
+       row_mapper(rest_row, restCode, [ pixel | row_mapped ], y, x+1)
+  end
+
+  def image_mapper([], code, poster, line), do: poster
+
+  def image_mapper([row | rest], restCode, line, poster) do
+      {row, code} = row_mapper(row, restCode, [], 0, line)
+      image_mapper(rest, code, line + 1 , [ row | poster ] )
+  end
+
+  def construct_text_elements(code, ratio, %{pixels: pixels}) do
       Logger.debug("Constructing text elements...")
-      text_elements = pixels
-      |> Enum.with_index
-      |> Enum.map(get_row_mapper(code, width, ratio))
-      |> List.flatten
-      %{data | text_elements: text_elements}
+      row_mapper(pixels, code, 0, [])
   end
 
 end
