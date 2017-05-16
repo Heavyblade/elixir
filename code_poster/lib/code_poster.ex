7d39d959929d79532bb2ed96a86c7c1f75b7d888
CristianV <cristian.vasquez@educate-online.com>
2017-05-16 11:55:26 -0500

    Adding agent state

--- code_poster/lib/code_poster.ex
+++ code_poster/lib/code_poster.ex
@@ -22,29 +22,29 @@ defmodule CodePoster do
   end
 
   def get_needed_code(code, %{width: width, height: height}) do
-      Stream.cycle(code)
-      |> Enum.take(width * height)
+       Agent.start_link fn -> Stream.cycle(code) |> Enum.take(width * height) end
   end
 
-  def construct_text_elements(code, %{pixels: pixels}) do
+  def construct_text_elements({:ok, code_pid}, %{pixels: pixels}) do
       Logger.debug("Constructing text elements...")
-      image_mapper(pixels, code, 0, [])
+      image_mapper(pixels, code_pid, 0, [])
   end
 
   def image_mapper([], _, _, poster), do: poster
 
-  def image_mapper([row | rest], code, line, poster) do
-      {row, rest_code} = row_mapper(row, code, [], 0, line)
-      image_mapper(rest, rest_code, line + 1 , poster ++ [row])
+  def image_mapper([row | rest], code_pid, line, poster) do
+      image_mapper(rest, code_pid, line + 1 , poster ++ [row_mapper(row, code_pid, [], 0, line)])
   end
 
-  def row_mapper([], restCode, row_mapped, _, _) do
-      {Enum.reverse(row_mapped), restCode}
+  def row_mapper([], _, row_mapped, _, _), do: Enum.reverse(row_mapped)
+
+  def row_mapper([pixel | rest_row], code_pid, row_mapped, x, y) do
+      pixel = pixed_mapper(pixel, code_pid, x, y)
+      row_mapper(rest_row, code_pid, [pixel | row_mapped], x+1, y)
   end
 
-  def row_mapper([pixel | rest_row], [chr | restCode], row_mapped, x, y) do
-      pixel = pixed_mapper(pixel, chr, x, y)
-      row_mapper(rest_row, restCode, [pixel | row_mapped], x+1, y)
+  def get_chr(code_pid) do
+      Agent.get_and_update(code_pid, fn [chr | restCode] -> {chr, restCode} end)
   end
 
   @doc """
@@ -52,8 +52,8 @@ defmodule CodePoster do
   iex> CodePoster.pixed_mapper({255, 0, 128}, "j", 1, 1)
   {:text, %{style: "fill: #FF0080;", x: 12, y: 40}, "j"}
   """
-  def pixed_mapper({r,g,b}, chr, x, y) do
-      { :text, %{x: round(x * @ratio * @fontSize), y: (round(@fontSize * y) + 20), style: "fill: #{ImageHandler.to_hex(r,g,b)};"}, chr }
+  def pixed_mapper({r,g,b}, code_pid, x, y) do
+      { :text, %{x: round(x * @ratio * @fontSize), y: (round(@fontSize * y) + 20), style: "fill: #{ImageHandler.to_hex(r,g,b)};"}, get_chr(code_pid) }
   end
 
   @doc """
@@ -61,8 +61,8 @@ defmodule CodePoster do
   iex> CodePoster.pixed_mapper({255, 0, 128, 250}, "j", 1, 1)
   {:text, %{opacity: "0.98", style: "fill: #FF0080;", x: 12, y: 40}, "j"}
   """
-  def pixed_mapper({r,g,b, a}, chr, x, y) do
-      { :text, %{x: round(x * @ratio * @fontSize), y: (round(@fontSize * y) + 20), style: "fill: #{ImageHandler.to_hex(r,g,b)};", opacity: "#{Float.round(a/255, 2)}"}, chr }
+  def pixed_mapper({r,g,b, a}, code_pid, x, y) do
+      { :text, %{x: round(x * @ratio * @fontSize), y: (round(@fontSize * y) + 20), style: "fill: #{ImageHandler.to_hex(r,g,b)};", opacity: "#{Float.round(a/255, 2)}"}, get_chr(code_pid) }
   end
 
 end
