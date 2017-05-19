6bbc1c9dd5e21f20bfe030339b3cd5f2044bb22b
CristianV <cristian.vasquez@educate-online.com>
2017-05-19 15:58:19 -0500

    otp

--- spawn.exs
+++ spawn.exs
@@ -1,22 +1,42 @@
-defmodule Spawn1 do
-  def greet do
-      receive do
-        {sender, msg} ->
-          send sender, {:ok, "Hello, #{msg}"}
-      end
-  end
+# defmodule Spawn1 do
+#   def greet do
+#       receive do
+#         {sender, msg} ->
+#           send sender, {:ok, "Hello, #{msg}"}
+#       end
+#   end
+# end
+#
+# defmodule Sender do
+#   def hello do
+#       pid = spawn(Spawn1, :greet, [])
+#
+#       send pid, {self, "Cristian"}
+#
+#       receive do
+#         {:ok, msg} -> IO.puts msg
+#       end
+#   end
+# end
+
+
+defmodule Test do
+    def hello do
+        receive do
+           {sender, msg} -> send sender, {:ok, msg}
+        end
+    end
 end
 
-defmodule Sender do
-  def hello do
-      pid = spawn(Spawn1, :greet, [])
+defmodule Caller do
+    def call(msg) do
+        pid = spawn(Test, :hello, [])
+        send pid, {self(), msg}
 
-      send pid, {self, "Cristian"}
-
-      receive do
-        {:ok, msg} -> IO.puts msg
-      end
-  end
+        receive do
+            {:ok, message} -> IO.puts message
+        end
+    end
 end
 
-Sender.hello
+Caller.call("Hello world")
